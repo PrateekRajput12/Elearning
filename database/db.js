2b5d11fdb09f91ec577686ae22d8188d85b676f9
PrateekRajput12 <ficredinn@gmail.com>
2025-12-28 10:20:12 +0530

    Setup completed

---
+++ database/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGODB_URL)
+
+        console.log("connected successfully");
+    } catch (error) {
+        console.log("Error in connecting DB");
+        console.log(error.message);
+    }
+}
+
+
+export default connectDB
--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 import express from "express";
 import dotenv from 'dotenv'
+import connectDB from "./database/db.js";
 
 dotenv.config({
     quiet: true // ----> Quite is for stop showing message of dot env in terminal
 })
 const app = express()
 const PORT = process.env.PORT || 5000
-
 app.get("/", (req, res) => {
     res.json({ message: "Running" })
 })
 app.listen(PORT, () => {
+
     console.log(`running on port ${PORT}`);
+    connectDB()
+
 })
